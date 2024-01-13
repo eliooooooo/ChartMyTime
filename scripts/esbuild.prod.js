20a14c2e289dda874b59816ce84625266a0be1dc
Eliooooooo <eliott.burkle@gmail.com>
2024-01-13 16:54:10 +0100

    new esbuild script

--- scripts/esbuild.prod.js
+++ scripts/esbuild.prod.js
@@ -1,30 +1,42 @@
+const fs = require('fs');
 const esbuild = require("esbuild");
 const postCssPlugin = require("esbuild-style-plugin");
 
-esbuild.build({
-  logLevel: "debug",
-  entryPoints: ["./public/src/js/main.js"],
-  outdir: "dist",
-  bundle: true,
-  minify: true, 
-  loader: {
-    // ".svg": "file",
-    // ".otf": "file",
-    // ".eot": "file",
-    // ".woff": "file",
-    // ".woff2": "file"
-  },
-  plugins: [
-    postCssPlugin({
-      postcss: {
-        plugins: [
-          require("postcss-import"),
-          require("tailwindcss/nesting"),
-          require("tailwindcss"),
-          require("autoprefixer")],
-      },
-    }),
-  ],
-})
-.then(result => { console.log('Js & css minifiés !!') })
-.catch(() => process.exit(1));
+fs.readdir('./public/src/js', (err, files) => {
+  if(err) {
+    console.error("erreur de lecture du dossier ./public/src/js");
+    process.exit(1);
+  }
+
+  const entryPoints = files
+    .filter(file => file.endsWith('.js'))
+    .map(file => `./public/src/js/${file}`);
+
+  esbuild.build({
+    logLevel: "debug",
+    entryPoints: entryPoints,
+    outdir: "dist",
+    bundle: true,
+    minify: true, 
+    loader: {
+      // ".svg": "file",
+      // ".otf": "file",
+      // ".eot": "file",
+      // ".woff": "file",
+      // ".woff2": "file"
+    },
+    plugins: [
+      postCssPlugin({
+        postcss: {
+          plugins: [
+            require("postcss-import"),
+            require("tailwindcss/nesting"),
+            require("tailwindcss"),
+            require("autoprefixer")],
+        },
+      }),
+    ],
+  })
+  .then(result => { console.log('Js & css minifiés !!') })
+  .catch(() => process.exit(1));
+});
--- scripts/esbuild.watch.js
+++ scripts/esbuild.watch.js
@@ -1,34 +1,92 @@
+// const esbuild = require('esbuild');
+// const postCssPlugin = require('esbuild-style-plugin');
+
+// (async () => {
+//   let ctx = await esbuild.context({
+//     logLevel: 'debug',
+//     entryPoints: ['./public/src/js/main.js'],
+//     outdir: 'dist',
+//     bundle: true, 
+//     minify: false,
+//     loader: {
+//       '.svg': 'file',
+//       '.otf': 'file',
+//       '.eot': 'file',
+//       '.woff': 'file',
+//       '.woff2': 'file'
+//     },
+//     plugins: [
+//       postCssPlugin({
+//         postcss: {
+//           plugins: [
+//             require('postcss-import'),
+//             require('tailwindcss/nesting'),
+//             require('tailwindcss'),
+//             require('autoprefixer')
+//           ],
+//         },
+//       }),
+//     ],
+//   })
+  
+//   await ctx.watch()
+// })();
+const fs = require('fs');
 const esbuild = require('esbuild');
 const postCssPlugin = require('esbuild-style-plugin');
+const chokidar = require('chokidar');
 
-(async () => {
-  let ctx = await esbuild.context({
-    logLevel: 'debug',
-    entryPoints: ['./public/src/js/main.js'],
-    outdir: 'dist',
-    bundle: true, 
-    minify: false,
-    loader: {
-      '.svg': 'file',
-      '.otf': 'file',
-      '.eot': 'file',
-      '.woff': 'file',
-      '.woff2': 'file'
-    },
-    plugins: [
-      postCssPlugin({
-        postcss: {
-          plugins: [
-            require('postcss-import'),
-            require('tailwindcss/nesting'),
-            require('tailwindcss'),
-            require('autoprefixer')
-          ],
+const build = () => {
+  fs.readdir('./public/src/js', (err, files) => {
+    if(err) {
+      console.error("erreur de lecture du dossier ./public/src/js");
+      process.exit(1);
+    }
+
+    const entryPoints = files
+      .filter(file => file.endsWith('.js'))
+      .map(file => `./public/src/js/${file}`);
+
+    entryPoints.forEach(entryPoint => {
+      esbuild.build({
+        entryPoints: [entryPoint],
+        outdir: "dist",
+        bundle: true,
+        minify: true, 
+        loader: {
+          // ".svg": "file",
+          // ".otf": "file",
+          // ".eot": "file",
+          // ".woff": "file",
+          // ".woff2": "file"
         },
-      }),
-    ],
-  })
-  
-  await ctx.watch()
-})();
+        plugins: [
+          postCssPlugin({
+            postcss: {
+              plugins: [
+                require("postcss-import"),
+                require("tailwindcss/nesting"),
+                require("tailwindcss"),
+                require("autoprefixer")],
+            },
+          }),
+        ],
+      }).catch(() => process.exit(1));
+    });
+  });
+}
 
+// Run build once at the start
+build();
+
+// Then watch for file changes in js files
+chokidar.watch('./public/src/js').on('change', (path) => {
+  console.log(`File ${path} has been changed`);
+  build();
+});
+
+// Then watch for file changes in scss files
+chokidar.watch('./public/src/css/**/*.scss').on('change', (path) => {
+  console.log(`File ${path} has been changed`);
+  build();
+});
