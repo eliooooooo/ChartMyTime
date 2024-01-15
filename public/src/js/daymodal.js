d07d23d40c68a8e9b59e2e988b3173c6b0e91752
Eliooooooo <eliott.burkle@gmail.com>
2024-01-15 17:55:06 +0100

    display selected date in modal

--- public/src/js/daymodal.js
+++ public/src/js/daymodal.js
@@ -4,13 +4,17 @@ export function dayModal(){
     let dayCards = document.querySelectorAll('.dayCard');
     let dayModal = document.querySelector('.dayModal');
     let dayModalClose = document.querySelector('.dayModalClose');
+    let modalDate = document.querySelector('.modalDate');
+    let modalMonth = document.querySelector('.modalMonth');
+    let activeMonth = document.querySelector('.month');
+    let activeYear = document.querySelector('.year');
     
     dayCards.forEach(function(dayCard) {
         dayCard.addEventListener('click' , function() {
             let dayNumber = dayCard.dataset.day;
             console.log('clicked day ' + dayNumber);
             dayModal.classList.remove('hidden');
-    
+            modalDate.innerHTML = dayNumber + ' ' + activeMonth.innerHTML + ' ' + activeYear.innerHTML;
         });
     });
 
