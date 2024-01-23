d2d14684656d539611e68ca3b1134c4da4e8634c
Eliooooooo <eliott.burkle@gmail.com>
2024-01-23 10:23:48 +0100

    base of user & workspace controller

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -5,4 +5,24 @@ Class UserController extends ControllerBase {
     function read(){
         $this->render('/page/user.html.twig');
     }
+
+    function create(){
+        $this->render('/page/user.html.twig');
+    }
+
+    function update(){
+        $this->render('/page/user.html.twig');
+    }
+
+    function delete(){
+        $this->render('/page/user.html.twig');
+    }
+
+    function login(){
+        $this->render('/page/login.html.twig');
+    }
+
+    function logout(){
+        $this->render('/page/login.html.twig');
+    }
 }
---
+++ app/controller/WorkspaceController.php
@@ -0,0 +1,20 @@
+<?php 
+
+Class WorkspaceController extends ControllerBase {
+
+    function read(){
+        $this->render('/components/calendar.html.twig');
+    }
+
+    function create(){
+        $this->render('/components/calendar.html.twig');
+    }
+
+    function update(){
+        $this->render('/components/calendar.html.twig');
+    }
+
+    function delete(){
+        $this->render('/components/calendar.html.twig');
+    }
+}
