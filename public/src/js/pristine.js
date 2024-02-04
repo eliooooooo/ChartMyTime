d57b7516761e031cc728233e5c63d64889615fa6
Eliott <eliott.burkle@gmail.com>
2024-02-04 04:35:16 +0100

    complex password front validation

--- public/src/js/pristine.js
+++ public/src/js/pristine.js
@@ -30,18 +30,59 @@ window.onload = function () {
  
     let pristine = new Pristine(formLogin);
     let pristine2 = new Pristine(formRegister, config);
+
+    const password = formRegister.querySelector('#password');
+
+    if (password && formRegister) {
+        const pristine = new Pristine(formRegister);
+        const formgroupPassword = document.querySelector(".formgroupPassword");
+        password.addEventListener("input", function(e){
+            console.log(formgroupPassword);
+            if (formgroupPassword.classList.contains("has-danger")){
+                if (password.value === "") {
+                    errorPassword.classList.add("hidden");
+                    errorPassword.classList.remove("text-help");
+                } else {
+                    console.log("toogle");
+                    const errorPassword = document.querySelector("#errorPassword");
+        
+                    errorPassword.classList.remove("hidden");
+                    errorPassword.classList.add("text-help");
+                }
+            } else {
+                errorPassword.classList.remove("text-help");
+                errorPassword.classList.add("hidden");
+            }
+        });
+
+        pristine.addValidator(password, function(value) {
+            return value.length >= 8;
+        }, "<span class='ml-4'>- At least 8 characters</span>", 2);
+
+        pristine.addValidator(password, function(value) {
+            return /[a-z]/.test(value);
+        }, "<span class='ml-4'>- At least one lowercase letter</span>", 2);
+
+        pristine.addValidator(password, function(value) {
+            return /[A-Z]/.test(value);
+        }, "<span class='ml-4'>- At least one uppercase letter</span>", 2);
+
+        pristine.addValidator(password, function(value) {
+            return /\d/.test(value);
+        }, "<span class='ml-4'>- At least one digit</span>", 2);
+    }
  
     formLogin.addEventListener('submit', function (e) {
        var valid = pristine.validate();
        if (!valid) {
-           e.preventDefault(); // Empêche la soumission du formulaire si la validation échoue
+           e.preventDefault();
        }
     });
 
     formRegister.addEventListener('submit', function (e) {
         var valid = pristine2.validate();
         if (!valid) {
-            e.preventDefault(); // Empêche la soumission du formulaire si la validation échoue
+            e.preventDefault(); 
         }
      });
 };
