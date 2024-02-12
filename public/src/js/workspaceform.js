a1336dfc846c92fe39a3aa862ea60fb864afbaec
Eliott <eliott.burkle@gmail.com>
2024-02-12 14:17:31 +0100

    responsive menu and workspaceForm

--- public/src/js/workspaceform.js
+++ public/src/js/workspaceform.js
@@ -1,8 +1,18 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    let workspaceForm = document.querySelector('#newWorkspace');
-    let submitButton = workspaceForm.querySelector('.submitWorkspace');
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     let workspaceForm = document.querySelector('#newWorkspace');
+//     let submitButton = workspaceForm.querySelector('.submitWorkspace');
 
-    submitButton.addEventListener('click', function(event) {
-        workspaceForm.submit();
+//     submitButton.addEventListener('click', function(event) {
+//         workspaceForm.submit();
+//     });
+// });
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    let submitButton = document.querySelectorAll(".submitWorkspace");
+    submitButton.forEach(element => {
+        element.addEventListener("click", function(event2) {
+            let workspaceForm = submitButton.parentNode;
+            workspaceForm.submit();
+        });
     });
 });
