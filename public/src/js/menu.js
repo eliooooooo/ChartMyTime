9bf0f26ee87c0443a2df85b0c9cd1dde737c5dc0
Eliott <eliott.burkle@gmail.com>
2024-02-25 17:59:51 +0100

    active state menu on create and delete

--- public/src/js/menu.js
+++ public/src/js/menu.js
@@ -5,6 +5,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
     navLinks.forEach((link) => {
         if (link.href === currentUrl) {
             link.classList.add('active');
+        } else if (currentUrl.includes('delete')){
+            let home = document.querySelectorAll('#home');
+            home.forEach(element => {
+                element.classList.add('active');
+            });
+        } else if (currentUrl.includes('create')) {
+            let workspaceList = document.querySelectorAll('#workspaceList');
+            let target = [];
+
+            workspaceList.forEach(element => {
+                if (element.children.length > 1) {
+                    target.push(element.children[element.children.length - 2]);
+                }
+            });
+
+            target.forEach(element => {
+                element.classList.add('active');
+            });
+            
+            console.log(target);
         }
     });
 });
