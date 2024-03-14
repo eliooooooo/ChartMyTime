6ab4e6354605f11ee3f9f93f77d145a0cf48333f
Eliott <eliott.burkle@gmail.com>
2024-03-14 14:14:30 +0100

    Form add day integration

--- chart-my-time/tailwind.config.js
+++ chart-my-time/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
         "./app/views/**/**/*.{html.twig,js}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                'xs': '470px',
+            }
+        },
     },
     plugins: [],
 } 
